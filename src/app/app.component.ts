e07b1fc9b2f3e6a3ea3f2a648029f2147d338caa
Ian Mwariri <mwangi.ian3@gmail.com>
2021-03-26 14:56:53 +0300

    Add an array of quotes with defined datatypes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:string[];
+  quotes:Quote[]= [
+    {id:1, saying: 'Welcome every morning with a smile:)'},
+    {id:2, saying: 'Celebrate the small wins!'},
+    {id:3, saying: 'Obssess about the process, not the results.'},
+    {id:4, saying: "Dont tell your friends about your indigestion. 'How are you' is a greeting, not a question;) "},
+    {id:5, saying: 'Keep it light-hearted always!'},
 
-  constructor(){
-    this.quotes = ['Welcome every morning with a smile:)', 'Celebrate the small wins!']
-  }
+  ];
 
 }
