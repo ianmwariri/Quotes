22a303209805ca0c341e26342df9eb89d029469d
Ian Mwariri <mwangi.ian3@gmail.com>
2021-03-26 21:10:50 +0300

    Implement 2-way Data Binding.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public id:number, public saying: string, public personSubmitting: string, public postingDate: Date){
+    constructor(public id:number, public saying: string, public author:string, public personSubmitting: string, public postingDate: Date){
         this.showDescription=false;
     }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,11 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]= [
-    new Quote(1, 'Welcome every morning with a smile:)', 'Ian', new Date(2020,3,14)),
-    new Quote(2, 'Celebrate the small wins!', 'Liz', new Date(2019,6,9)),
-    new Quote(3, 'Obssess about the process, not the results.', 'Isabella', new Date(2018,1,12)),
-    new Quote(4, "Dont tell your friends about your indigestion. 'How are you' is a greeting, not a question;) " , 'James', new Date(2017,0,18)),
-    new Quote(5, 'Keep it light-hearted always!', 'Arthur', new Date(2021,1,14)),
+    new Quote(1, 'Welcome every morning with a smile:)', 'Ian', 'Steve Wonder', new Date(2020,3,14)),
+    new Quote(2, 'Celebrate the small wins!', 'Liz', 'Craig Groeschel', new Date(2019,6,9)),
+    new Quote(3, 'Obssess about the process, not the results.', 'Isabella', 'Martin King', new Date(2018,1,12)),
+    new Quote(4, "Dont tell your friends about your indigestion. 'How are you' is a greeting, not a question;) " , 'James', 'Arthur Guit', new Date(2017,0,18)),
+    new Quote(5, 'Keep it light-hearted always!', 'Arthur','Jason Mitchel', new Date(2021,1,14)),
     
   ];
 
