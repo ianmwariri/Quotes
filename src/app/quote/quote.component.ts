24ea010f593a1dea7f08574d27e48b2f5e478108
Ian Mwariri <mwangi.ian3@gmail.com>
2021-03-26 15:49:31 +0300

    Move Default app component logic to Custom Quote Component.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes:Quote[]= [
+    {id:1, saying: 'Welcome every morning with a smile:)'},
+    {id:2, saying: 'Celebrate the small wins!'},
+    {id:3, saying: 'Obssess about the process, not the results.'},
+    {id:4, saying: "Dont tell your friends about your indigestion. 'How are you' is a greeting, not a question;) "},
+    {id:5, saying: 'Keep it light-hearted always!'},
+    
+  ];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
