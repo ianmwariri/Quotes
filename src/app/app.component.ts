d4ace2549458e52935f7bf78410ce8e2426f3822
Ian Mwariri <mwangi.ian3@gmail.com>
2021-03-26 14:20:38 +0300

    Create Quotes App Project Folder.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'Welcome every morning with a smile:)'
 }
